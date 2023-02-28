9620f8a405f822a81688c7e5fbe399012b7a5311
HakunaMatata <kononchuk.elena91@gmail.com>
2023-02-28 17:20:19 +0200

    mobile menu done

--- files/js/main.js
+++ files/js/main.js
@@ -9,3 +9,36 @@
       e.currentTarget.reset();
    });
 })();
+
+// popup gifts
+(() => {
+   const refs = {
+      openModalBtn: document.querySelector("[data-open]"),
+      closeModalBtn: document.querySelector("[data-close]"),
+      modal: document.querySelector("[modal]"),
+   };
+
+   refs.openModalBtn.addEventListener("click", toggleModal);
+   refs.closeModalBtn.addEventListener("click", toggleModal);
+
+   function toggleModal() {
+      // document.body.classList.toggle("modal-open");
+      refs.modal.classList.toggle("is-hidden");
+   }
+})();
+// popup partner
+(() => {
+   const refs = {
+      openModalBtn: document.querySelector("[data-modal-open]"),
+      closeModalBtn: document.querySelector("[data-modal-close]"),
+      modal: document.querySelector("[data-modal]"),
+   };
+
+   refs.openModalBtn.addEventListener("click", toggleModal);
+   refs.closeModalBtn.addEventListener("click", toggleModal);
+
+   function toggleModal() {
+      // document.body.classList.toggle("modal-open");
+      refs.modal.classList.toggle("is-hidden");
+   }
+})();
