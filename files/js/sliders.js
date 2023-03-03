984fe4a4fa01f7c7055f2daffb846bd277507390
HakunaMatata <kononchuk.elena91@gmail.com>
2023-03-03 19:14:09 +0200

    finish section works/benefits/partners,1 slider for works sections

---
+++ files/js/sliders.js
@@ -0,0 +1,43 @@
+new Swiper('.works-page__slider', {
+   // Navigation arrows
+   navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+   },
+   // Optional parameters
+
+   loop: true,
+
+   // If we need pagination
+   pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+   },
+
+   // And if we need scrollbar
+   scrollbar: {
+      el: '.swiper-scrollbar',
+   },
+});
+
+new Swiper('.partners-page__slider', {
+   // Navigation arrows
+   navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+   },
+   // Optional parameters
+
+   loop: true,
+
+   // If we need pagination
+   // pagination: {
+   //    el: '.swiper-pagination',
+   //    clickable: true,
+   // },
+
+   // And if we need scrollbar
+   // scrollbar: {
+   //    el: '.swiper-scrollbar',
+   // },
+});
