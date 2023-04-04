664f2c999b7cec8a0078f7ff16cafebe9df72e49
HakunaMatata <kononchuk.elena91@gmail.com>
2023-04-04 22:34:04 +0300

    create slider with coverflow effect

--- files/js/sliders.js
+++ files/js/sliders.js
@@ -1,26 +1,47 @@
 // works section swiper
-let swiper1 = new Swiper('.works-page__slider', {
-   // Navigation arrows
-   navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+const swiper1 = new Swiper('.works-page__slider', {
+   loop: false,
+   speed: 500,
+   effect: 'coverflow',
+   coverflowEffect: {
+      rotate: 0,
+      slideShadows: false,
    },
+   initialSlide: 3,
+
    // Optional parameters
-
-
-
+   centeredSlides: true,
    // If we need pagination
    pagination: {
       el: '.swiper-pagination',
       clickable: true,
    },
 
+   // Navigation arrows
+   navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+   },
 
    // And if we need scrollbar
-   scrollbar: {
-      el: '.swiper-scrollbar',
+
+   breakpoints: {
+      320: {
+         slidesPerView: 1,
+         spaceBetween: 40,
+      },
+      1000: {
+         slidesPerView: 2,
+         spaceBetween: 40,
+      },
+      1440: {
+         slidesPerView: 2,
+         spaceBetween: 40,
+      },
    },
 });
+
+
 // partners section swiper
 let swiper2 = new Swiper('.partners-page__slider', {
    slidesPerView: 2,
