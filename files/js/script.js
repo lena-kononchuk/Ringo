b9fae7ea8c84bb99cb322572e5081e418ee08def
HakunaMatata <kononchuk.elena91@gmail.com>
2023-04-14 01:50:59 +0300

    finish validete

--- files/js/script.js
+++ files/js/script.js
@@ -12,68 +12,41 @@
 	});
 })();
 
-
-
 document.addEventListener('DOMContentLoaded', () => {
-	let validation = new JustValidate('#form',);
-	let selector = document.querySelector("input[type='tel']");
-	let im = new Inputmask("+38(099)-999-99-99");
+	// const validation = new JustValidate('.js-speaker-form');
 
-	im.mask(selector);
+	// create error/validation
+	// Получаем все формы на странице
+	const forms = document.querySelectorAll('.form');
 
-	validation.addField(".js-input-name", [
-		{
-			rule: 'required',
-			errorMessage: "Введіть ім'я"
-		},
-		{
-			rule: 'customRegexp',
-			value: /^[a-zA-Zа-яА-Я]+$/gi,
-			errorMessage: "Мають бути тільки букви"
-		},
-		{
-			rule: 'minLength',
-			value: 3,
-			errorMessage: "Мінімум 3 літери"
-		},
-		{
-			rule: 'maxLength',
-			value: 15,
-			errorMessage: "Максимум 15 літер"
-		},
-	])
-		// .addField(".js-input-checkbox", [
-		// 	{
-		// 		rule: 'required',
-		// 		errorMessage: "jhdfjdsdfshh"
-		// 	},
-		// ])
-		.addField('.js-input-phone', [
+	// Проходимся по каждой форме и создаем экземпляр JustValidate для каждой из них
+	for (const form of forms) {
+		const formValidator = new JustValidate(`form#${form.id}`);
+		// Inputmask code
+		let selector = document.querySelectorAll("input[type='tel']");
+		let im = new Inputmask("+38(099)-999-99-99");
+		im.mask(selector);
+
+		// Добавляем правила валидации для каждой формы
+		formValidator.addField(".js-input-name", [
 			{
 				rule: 'required',
-				errorMessage: "Введіть телефон"
+				errorMessage: "Введіть ім'я"
 			},
 			{
 				rule: 'customRegexp',
-				value: /^[\s()+-]*([0-9][\s()+-]*){6,20}$/gi,
-				errorMessage: "Тільки цифри"
+				value: /^[a-zA-Zа-яА-Я]+$/gi,
+				errorMessage: "Мають бути тільки букви"
 			},
 			{
 				rule: 'minLength',
-				value: 10,
-				errorMessage: "Мінімум 10 символів"
+				value: 3,
+				errorMessage: "Мінімум 3 літери"
 			},
+		]).addField('.js-input-phone', [
 			{
-				rule: 'maxLength',
-				value: 19,
-				errorMessage: "Максимум 13 символів"
-			},
-			{
-				validator: (name, value) => {
-					let phone = selector.inputmask.unmaskedvalue()
-					return Number(phone) && phone.length === 9
-				},
-				errorMessage: 'Недостаточное количество знаков',
+				rule: 'required',
+				errorMessage: "Введіть телефон"
 			},
 		]).onFail(fields => {
 
@@ -83,19 +56,41 @@ document.addEventListener('DOMContentLoaded', () => {
 				field.value = "";
 			}
 		});
-	let submitBtn = document.querySelector('button[type="submit"]');
-	let agreeCheckbox = document.querySelector('input[name="check"]');
-	validation.addField("#checkbox", [
-		{
-			rule: 'required',
-			errorMessage: "Максимум 13 символів"
-		},
-	])
-	agreeCheckbox.addEventListener('click', function () {
-		if (this.checked) {
-			submitBtn.disabled = false;
+	}
+});
+// check the policy
+function isCheck() {
+
+	for (const form of submitBtns) {
+		let label = document.querySelector('.policy__label');
+		let labelIcon = document.querySelector('.policy__icon');
+		let checks = document.getElementById(`form#${form.id}`);
+		var submitBtns = document.getElementById(`form#${form.id}`);
+		if (check.checked) {
+			submitBtns.disabled = false;
+			label.textContent = "Даю згоду на оброблення персональних даних";
+			label.style.color = '#cc9654';
+			labelIcon.style.fill = "#cc9654";
 		} else {
 			submitBtn.disabled = true;
 		}
-	});
-});
+	}
+}
+// function isCheck() {
+// 	const form = document.querySelectorAll('.form');
+// 	let label = document.querySelector('.policy__label');
+// 	let labelIcon = document.querySelector('.policy__icon');
+// 	let checks = document.getElementById(`form#${form.id}`);
+// 	var submitBtns = document.getElementById(`form#${form.id}`);
+// 	for (const form of submitBtns) {
+// 		if (checks.checked) {
+// 			submitBtns.disabled = false;
+// 			label.textContent = "Даю згоду на оброблення персональних даних";
+// 			label.style.color = '#cc9654';
+// 			labelIcon.style.fill = "#cc9654";
+// 		} else {
+// 			submitBtns.disabled = true;
+// 		}
+// 	}
+// }
+
